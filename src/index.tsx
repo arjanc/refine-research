dc80bb14c493fcda745fa92cfedee3709b582ee2
Arjan Ceelen <arjan@celar.nl>
2023-08-22 16:29:00 +0200

    app loading indicator

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Space, Spin } from 'antd';
 
 import App from "./App";
 import "./i18n";
@@ -9,8 +10,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback="loading">
+    <React.Suspense fallback={<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}><Space align="center"><Spin size="large"/></Space></div>}>
       <App />
     </React.Suspense>
   </React.StrictMode>
 );
+
